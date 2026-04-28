38501e8da9e05f74b2db8a3a65a696e8647ee3ef
maloltzahi-byte <maloltzahi@gmail.com>
2026-04-28 14:53:22 +0300

    Extract LogoBlock layout component

---
+++ src/components/layout/LogoBlock.tsx
@@ -0,0 +1,5 @@
+import { Icon } from '../shared/Icon'
+
+export function LogoBlock() {
+  return <div className="logo"><div className="brand"><b>קדישים</b><small>לזכרם. בנוכחות</small></div><Icon name="flame" size={26} className="logo-mark" /></div>
+}

1c2cb1d200f566beed89585b66c6a52a5c5e54f3
maloltzahi-byte <maloltzahi@gmail.com>
2026-04-28 13:51:25 +0300

    Add Pagination table component

---
+++ src/components/table/Pagination.tsx
@@ -0,0 +1,3 @@
+export function Pagination() {
+  return <div className="pagination"><span>הצגה 1-5 מתוך 128</span><div className="pages"><button>הבא ←</button><button>5</button><button>4</button><button>3</button><button>2</button><button className="current">1</button><button>→ קודם</button></div></div>
+}

786ea45e63f6a5520df9ea18edf793f89f0b556a
maloltzahi-byte <maloltzahi@gmail.com>
2026-04-28 15:08:18 +0300

    Add call details route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { PageShell } from './components/layout/PageShell'
 import { Dashboard } from './pages/Dashboard'
 import { CallsList } from './pages/CallsList'
+import { CallDetailsPage } from './pages/CallDetailsPage'
 import { KaddishRequestsList } from './pages/KaddishRequestsList'
 import { VolunteersList } from './pages/VolunteersList'
 import { MembersList } from './pages/MembersList'
@@ -28,6 +29,10 @@ function SettingsPageRoute() {
   return <PageShell><SettingsPage /></PageShell>
 }
 
+function CallDetailsPageRoute() {
+  return <PageShell><CallDetailsPage /></PageShell>
+}
+
 export default function App() {
-  return <BrowserRouter><Routes><Route path="/" element={<Dashboard />} /><Route path="/calls" element={<CallsList />} /><Route path="/kaddish-requests" element={<KaddishRequestsPage />} /><Route path="/volunteers" element={<VolunteersPage />} /><Route path="/members" element={<MembersPage />} /><Route path="/reports" element={<ReportsPageRoute />} /><Route path="/settings" element={<SettingsPageRoute />} /></Routes></BrowserRouter>
+  return <BrowserRouter><Routes><Route path="/" element={<Dashboard />} /><Route path="/calls" element={<CallsList />} /><Route path="/calls/:callId" element={<CallDetailsPageRoute />} /><Route path="/kaddish-requests" element={<KaddishRequestsPage />} /><Route path="/volunteers" element={<VolunteersPage />} /><Route path="/members" element={<MembersPage />} /><Route path="/reports" element={<ReportsPageRoute />} /><Route path="/settings" element={<SettingsPageRoute />} /></Routes></BrowserRouter>
 }

9acab01f1d5d0f2149c09e56b7f5a2bdad5d639f
maloltzahi-byte <maloltzahi@gmail.com>
2026-04-29 11:43:10 +0300

    Import Sprint 31 public home styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './index.css'
 import './calls-patch.css'
 import './public.css'
+import './public-home.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
